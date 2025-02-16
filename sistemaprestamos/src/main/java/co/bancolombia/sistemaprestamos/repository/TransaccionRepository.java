c8d627cbcabe902ce14e8f597c54633593078953
DJBUSTAM <djbustam@bancolombia.com.co>
2025-02-16 12:21:12 -0500

    Entrega Taller Repaso

---
+++ sistemaprestamos/src/main/java/co/bancolombia/sistemaprestamos/repository/TransaccionRepository.java
@@ -0,0 +1,7 @@
+package co.bancolombia.sistemaprestamos.repository;
+
+import co.bancolombia.sistemaprestamos.model.Transaccion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransaccionRepository extends JpaRepository<Transaccion, Long> {
+}
